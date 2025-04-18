1caac7f64989c80c60f7c52bc5c3295b7f7f0822
Mohamed Elsaed <m.ashraf@converted.in>
2025-04-18 17:56:55 +0200

    dashboard

--- src/migrations/1744673749860-AddIpAddressToUsers.ts
+++ src/migrations/1744673749860-AddIpAddressToUsers.ts
@@ -4,14 +4,14 @@ export class AddIpAddressToUsers1744673749860 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE users
-      ADD COLUMN ipAddress varchar(45);
+      ADD COLUMN "ipAddress" varchar(45);
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE users
-      DROP COLUMN ip_address;
+      DROP COLUMN ipaddress;
     `);
   }
 }

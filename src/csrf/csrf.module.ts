b74440343294e49100eb830d87b628a50a596ac6
Mohamed Elsaed <m.ashraf@converted.in>
2025-04-18 04:33:58 +0200

    dashboard

--- src/csrf/csrf.module.ts
+++ src/csrf/csrf.module.ts
@@ -6,8 +6,10 @@ import {
   RequestMethod,
 } from '@nestjs/common';
 import { CsrfService } from './csrf.service';
+import { CsrfController } from './csrf.controller';
 
 @Module({
+  controllers: [CsrfController],
   providers: [CsrfService],
   exports: [CsrfService],
 })
